077f62b8b28decac659d405d4f3aa1933b06b40f
yongchang he <hyc0812@163.com>
2022-07-15 16:02:21 -0600

    added screenshot component

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -19,9 +19,12 @@ export const searchUsers = async (text) => {
 
 // Get user and repos
 export const getUserAndRepos = async (login) => {
+  const params = new URLSearchParams({
+    sort: "created",
+  });
   const [user, repos] = await Promise.all([
     github.get(`/users/${login}`),
-    github.get(`/users/${login}/repos`),
+    github.get(`/users/${login}/repos?${params}`),
   ]);
   return { user: user.data, repos: repos.data };
 };
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -55,14 +55,14 @@ function User() {
 
         <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8">
           <div className="custom-card-image mb-6 md:mb-0">
-            <div className="rounded-lg shadow-xl card image-full">
+            <div className="rounded-lg shadow-2xl card ">
               <figure>
                 <img src={avatar_url} alt="" />
               </figure>
-              <div className="card-body justify-end">
+              {/* <div className="card-body justify-end">
                 <h2 className="card-title mb-0">{name}</h2>
                 <p className="flex-grow-0">{login}</p>
-              </div>
+              </div> */}
             </div>
           </div>
 
