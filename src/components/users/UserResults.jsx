225d464c71683ac48f9a0e9a764b80199183704f
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-07-14 20:57:01 -0600

    spinner added

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Spinner from "../layout/Spinner";
 
 export default function UserResults(props) {
   const [users, setUsers] = useState([]);
@@ -19,14 +20,16 @@ export default function UserResults(props) {
 
     setUsers(data);
     setLoading(false);
-    console.log(data);
   };
-
-  return (
-    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-      {users.map((user) => (
-        <h3>{user.login}</h3>
-      ))}
-    </div>
-  );
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 }
