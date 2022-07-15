f9a9a3a5ce6e9275054a0de3b4b73b84dc0d901e
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-07-14 23:25:17 -0600

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import UserResults from "../components/users/UserResults";
+import UserSearch from "../components/users/UserSearch";
 
 export default function Home(props) {
   return (
     <div>
+      <UserSearch />
       <UserResults />
     </div>
   );
